4aaf8c7e9fa9ea469e6899b8731f588a427bffba
Saumya Kushwaha <190989046+saumya-2005@users.noreply.github.com>
2026-01-04 16:01:33 +0530

    Fix: import os for STATIC_ROOT

--- backend/radiology_backend/settings.py
+++ backend/radiology_backend/settings.py
@@ -4,6 +4,7 @@ Django settings for radiology_backend project.
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
