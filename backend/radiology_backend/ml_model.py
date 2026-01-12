9d62f7b4d45ad695d9552d465aae137b9b10eb8d
mahera8hub <syedmahera8102005@gmail.com>
2026-01-12 23:37:15 +0530

    updated api views, lazy loading model, requirements

--- backend/radiology_backend/ml_model.py
+++ backend/radiology_backend/ml_model.py
@@ -1,12 +1,13 @@
 # ml_model.py
 import tensorflow as tf
-
+BASE_DIR = Path(__file__).resolve().parent.parent
+MODEL_PATH = BASE_DIR / "model" / "efficientnet_finetuned_brain_tumor.h5"
 _model = None
 
 def get_model():
     global _model
     if _model is None:
-        _model = tf.keras.models.load_model("model.h5")  # path to your model
+        _model = tf.keras.models.load_model("model.h5", compile=False)  # path to your model
     return _model
 
 # from django.conf import settings
