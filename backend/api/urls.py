142c7660703e461526fd50fdc8c875f513d38911
mahera8hub <syedmahera8102005@gmail.com>
2026-01-14 16:34:17 +0530

    updated api/urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,11 @@
+from django.http import JsonResponse
 from django.urls import path
 from .views import predict_tumor
+def health(request):
+    return JsonResponse({"status": "Backend alive"})
 
 urlpatterns = [
+    path("", health),               # 👈 root route
     path('predict/', predict_tumor, name='predict_tumor'),
     #  path("doctor/signup/", DoctorSignupView.as_view(), name="doctor-signup"),
 ]
